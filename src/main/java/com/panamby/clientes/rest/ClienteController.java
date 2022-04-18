2466035ca66f1f45bc908b8414fc58aace72b46e
Rafael <rafa.jo.souza@hotmail.com>
2022-04-18 10:16:14 -0300

    Tratando de CORS

--- src/main/java/com/panamby/clientes/rest/ClienteController.java
+++ src/main/java/com/panamby/clientes/rest/ClienteController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.panamby.clientes.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
 	@Autowired
