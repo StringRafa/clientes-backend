88f98c1a157eebbd73390d977816db4dd5ef20e7
Rafael <rafa.jo.souza@hotmail.com>
2022-04-25 10:02:54 -0300

    Metodo de buscar servicos prestados

--- src/main/java/com/panamby/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/com/panamby/clientes/rest/ServicoPrestadoController.java
@@ -2,11 +2,14 @@ package com.panamby.clientes.rest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -49,4 +52,9 @@ public class ServicoPrestadoController {
 		return prestadoRepository.save(servicoPrestado);
 	}
 
+	@GetMapping
+	public List<ServicoPrestado> find(@RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+			@RequestParam(value = "mes", required = false) Integer mes){
+		return prestadoRepository.findByNomeClienteAndMes("%" + nome + "%", mes);
+	}
 }
