d165287cba6a3c97e5456c87d7305cc989ebd354
Rafael <rafa.jo.souza@hotmail.com>
2022-04-26 13:09:09 -0300

    Criando a classe WebConfig e tratando do cors

--- src/main/java/com/panamby/clientes/rest/ClienteController.java
+++ src/main/java/com/panamby/clientes/rest/ClienteController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +21,6 @@ import com.panamby.clientes.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/api/clientes")
-@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 	
 	@Autowired
