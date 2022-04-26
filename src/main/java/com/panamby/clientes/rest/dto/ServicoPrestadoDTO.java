a83af94adbb983e98c0c3f9acc10a161b99d6b1c
Rafael <rafa.jo.souza@hotmail.com>
2022-04-26 14:49:22 -0300

    Validando campos com mensagens de erro

--- src/main/java/com/panamby/clientes/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/com/panamby/clientes/rest/dto/ServicoPrestadoDTO.java
@@ -1,5 +1,8 @@
 package com.panamby.clientes.rest.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +10,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
 
+	@NotEmpty(message = "O campo Descrição é de preenchimento obrigatório")
 	private String descricao;
+	
+	@NotEmpty(message = "O campo Preço é de preenchimento obrigatório")
 	private String preco;
+	
+	@NotEmpty(message = "O campo Data é de preenchimento obrigatório")
 	private String data;
+	
+	@NotNull(message = "O campo Data é de preenchimento obrigatório")
 	private Long idCliente;
 }
